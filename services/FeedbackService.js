23a99fd6b3b88f6de721488e9bf9d18a2d046176
chanapa <chanapakrtp@gmail.com>
2022-10-25 19:58:47 +0700

    add feedback service

---
+++ services/FeedbackService.js
@@ -0,0 +1,21 @@
+const FeedbackModel = require("../models/Feedback");
+
+exports.getAllFeedbacks = async () => {
+  return await FeedbackModel.find();
+};
+
+exports.createFeedback = async (feedback) => {
+  return await FeedbackModel.create(feedback);
+};
+
+exports.getFeedbackById = async (id) => {
+  return await FeedbackModel.findById(id);
+};
+
+exports.updateFeedback = async (id, feedback) => {
+  return await FeedbackModel.findByIdAndUpdate(id, feedback);
+};
+
+exports.deleteFeedback = async (id) => {
+  return await FeedbackModel.findByIdAndDelete(id);
+};
